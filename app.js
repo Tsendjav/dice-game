4f77bd18cc1aeaac93b6db8d574cd2dd95a58a78
tsendee <tsendjav@outlook.com>
2020-09-25 22:05:34 +0800

    new game tovch belen bolgoh

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
-// Тоглогчын ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогч 0, хоёругаар тоглогч 1,
-var activePlayer = 0;
-
-// Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
-var scores = [0, 0];
-
-// Тоглогчын ээлжиндээ цуглуулж буй оноог хадгалах хувьсагч
-var roundScore = 0;
-
-// Програм эхлэхэд бэлтгэе
-
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-
+// Бүх ашиглагдах глобал хувсагчдыг энд зарлая
+// Идэвхтэй тоглогчыг энд зааж өгнө
+var activePlayer;
+// Хоёр тоглогын нийт оноог заана
+var scores;
+//Тухайн ээлжинд шоо хаяж буй нийт оноог заана
+var roundScore;
+// Шооны зургыг үзүүлэх элементийг ДОМ-с хайж олоод энд хадгалья
 var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none";
+
+// Тоглоомыг эхлүүлнэ
+initGame();
+
+// Тоглоомын шинээр эхлэхэд бэлтгэнэ
+function initGame() {
+  // Тоглогчын ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогч 0, хоёругаар тоглогч 1,
+  activePlayer = 0;
+
+  // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+  scores = [0, 0];
+
+  // Тоглогчын ээлжиндээ цуглуулж буй оноог хадгалах хувьсагч
+  roundScore = 0;
+
+  // Програм эхлэхэд бэлтгэе
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  // Тоглогчын нэрийг буцаах
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+
+  diceDom.style.display = "none";
+}
+
+// Тоглогчын ээлжийг солих
+function switchToNextPlayer() {
+  roundScore = 0;
+  document.getElementById("current-" + activePlayer).textContent = 0;
+
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  document
+    .querySelector(".player-" + activePlayer + "-panel")
+    .classList.toggle("active");
+  diceDom.style.display = "none";
+}
 
 //Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
@@ -45,28 +83,19 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
   scores[activePlayer] = scores[activePlayer] + roundScore;
   document.getElementById("score-" + activePlayer).textContent =
     scores[activePlayer];
+
   // Уг тоглогчын хожсон эсэхийг шалгах
-  if (scores[activePlayer] >= 20) {
+  if (scores[activePlayer] >= 10) {
     document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
-    document.querySelector("player-" + activePlayer + "-panel").classList.add =
+    document.querySelector(".player-" + activePlayer + "-panel").classList.add =
       "winner";
+    document.querySelector(
+      ".player-" + activePlayer + "-panel"
+    ).classList.remove = "active";
   } else {
     switchToNextPlayer();
   }
 });
 
-function switchToNextPlayer() {
-  roundScore = 0;
-  document.getElementById("current-" + activePlayer).textContent = 0;
-
-  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-  document
-    .querySelector(".player-" + activePlayer + "-panel")
-    .classList.toggle("active");
-  diceDom.style.display = "none";
-}
-
-// Шинэ тоглоом эхлүүлэх эвент листенер
-document.querySelector(".btn-new").addEventListener("click", function () {
-  alert("clicked");
-});
+// New Game буюу Шинэ тоглоом эхлүүлэх эвент листенер
+document.querySelector(".btn-new").addEventListener("click", initGame);
